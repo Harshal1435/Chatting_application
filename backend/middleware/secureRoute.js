19e35bde096b44feac1e9fe4f143e8b9e5203600
Harshal <raghatateharshal4@gmail.com>
2025-07-02 16:24:26 +0530

    fix: Enhance token retrieval in secureRoute to include authorization header fallback

--- backend/middleware/secureRoute.js
+++ backend/middleware/secureRoute.js
@@ -3,7 +3,7 @@ import User from "../models/user.model.js";
 
 const secureRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies.jwt||req.headers.authorization?.split(" ")[1]; // Get token from cookies or headers
     if (!token) {
       return res.status(401).json({ error: "No token, authorization denied" });
     }
