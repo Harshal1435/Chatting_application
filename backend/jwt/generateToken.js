800a2ec9b872871c0489ae17c9063337c36a5639
Harshal <raghatateharshal4@gmail.com>
2025-07-02 15:14:32 +0530

    feat: Store user ID and token in local storage upon token creation

--- backend/jwt/generateToken.js
+++ backend/jwt/generateToken.js
@@ -4,6 +4,7 @@ const createTokenAndSaveCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
     expiresIn: "10d",
   });
+  localStorage.setItem("ChatApp", JSON.stringify({ userId, token }));
   res.cookie("jwt", token, {
     httpOnly: true, // xss
     secure: true,
