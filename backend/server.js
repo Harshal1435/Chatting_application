d2636cb7a6a363cca0096c91c08875e57d668952
harshal <raghatateharshal4@gmail.com>
2025-12-18 21:56:44 +0530

    fix: Refactor CORS middleware to use a dynamic origin callback

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,26 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(cors({
-  origin: ["http://localhost:5173", "https://chatting-application-mu.vercel.app" , "https://chatting-application-1.netlify.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://chatting-application-mu.vercel.app",
+        "https://chatting-application-1.netlify.app",
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 const URI = process.env.MONGODB_URI;
